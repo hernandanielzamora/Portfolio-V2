77acac724d7497f1da1ac33dfdbd20e0d2baefaf
Hernan Zamora <hernanzamora1999@gmail.com>
2023-09-25 10:02:01 -0300

    Created first structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Summary from "./components/Summary";
+import Projects from "./components/Projects";
+import AboutMe from "./components/AboutMe";
+import Skills from "./components/Skills";
+import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+          <Summary />
+          <Projects />
+          <AboutMe />
+          <Skills />
+          <ContactForm />
+        </header>
+      </div>
+    </>
   );
 }
 
